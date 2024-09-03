799455b3c4c1a778d1d35af89c2ae3921ecbf09b
spahicadis <adis@spahica.com>
2024-09-03 22:43:01 +0200

    Project skoro gotov, taktimo prvo ga udaram nogama sa svih strana da radi pa onda na kraju sminkanje

--- app.js
+++ app.js
@@ -1,8 +1,50 @@
 //DOM
+const arrow = document.querySelector('.arrow');
+const items = document.querySelector('.menuItemsNone');
+const socialMedia = document.querySelectorAll('.js-menuItem')
+const activeMedia = document.querySelector('.displayActiveMedia')
  
 
 
 //STATES
 
 
+
 //BINDING
+
+function arrowStatus() {
+  let up = arrow.classList.contains('arrowDomUp');
+  if(!up) {
+    arrow.classList.remove('arrowDomDown');
+    arrow.classList.add('arrowDomUp');
+    items.classList.remove('menuItemsNone');
+    items.classList.add('menuItemsVisible');
+  } else {
+    arrow.classList.remove('arrowDomUp');
+    arrow.classList.add('arrowDomDown');
+    items.classList.remove('menuItemsVisible');
+    items.classList.add('menuItemsNone');
+  }
+}
+
+
+arrow.addEventListener('click', () => {
+  
+arrowStatus()
+  
+
+  
+});
+
+socialMedia.forEach((media) => {
+  media.addEventListener('mouseover', () => {
+    media.classList.add('mediaHover')
+  })
+})
+
+socialMedia.forEach((media) => {
+  media.addEventListener('click', () => {
+    activeMedia.innerHTML = media.innerHTML
+    
+  })
+})
